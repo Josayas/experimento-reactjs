6c6f4ca9149ccabc6cc3ba049160632713e5e8af
Josias <josias.sanhueza@outlook.com>
2018-02-02 19:15:56 -0300

    integración ws de reddit gift

--- src/Person.js
+++ src/Person.js
@@ -4,13 +4,43 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Person extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { kind: [] }
+  }
+
+  componentWillMount() {
+    fetch('https://www.reddit.com/r/gifs/.json')
+    .then((response) => {
+      return response.json()
+    }).then((data) => {
+      this.setState({kind: data.data.children})
+    })
+  }
+
+  
   render() {
-    return (
-      <div className="card" style={style}>
-        <h1>{this.props.name}</h1>
-        <img className="img-responsive" src={this.props.img} alt="logo" width="100px" />
-      </div>
-    );
+    var redditList = this.state.kind.map(function(e){
+      return (
+        <div className="card" style={style}>
+          <h6>{e.data.title}</h6>
+          <span className="text-truncate">{e.data.selftext}</span>
+          {/* <img className="img-responsive" src={this.props.img} alt="logo" width="100px" /> */}
+          <div className=" text-left"><a href={e.data.url} target="_blank">Read more...</a></div>
+          <div className="font-italic text-right">{e.data.author}</div>
+        </div>
+      );
+    });
+
+    if (this.state.kind.length > 0) {
+      return (
+        <div>
+          {redditList}
+        </div>
+      );
+    }else{
+      return <p className="text-center">Cargando reddit...</p>
+    }
   }
 }
 
